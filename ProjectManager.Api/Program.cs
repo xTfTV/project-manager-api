9624450e03e21dc4fdaf9a514e3bf27ac8c47776
xTfTV <mscott2003@icloud.com>
2026-09-16 21:53:36 -0400

    ft ✨ - Registering the DB configuration

--- ProjectManager.Api/Program.cs
+++ ProjectManager.Api/Program.cs
@@ -1,9 +1,16 @@
 
+using ProjectManager.Api.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add the services to the container
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+// Adding the DB Connection options
+builder.Services.Configure<DatabaseOptions>(
+    builder.Configuration.GetSection(DatabaseOptions.SectionName)
+);
 
 var app = builder.Build();
 
