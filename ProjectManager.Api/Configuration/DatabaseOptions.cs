dfd0ee4c2da4a5145e0be44e63c4bf04621e1b7f
xTfTV <mscott2003@icloud.com>
2026-09-16 21:49:31 -0400

    ft ✨ - Adding the API appsettings.json skeleton for the DB Connection string

---
+++ ProjectManager.Api/Configuration/DatabaseOptions.cs
@@ -0,0 +1,8 @@
+namespace ProjectManager.Api.Configuration;
+
+public class DatabaseOptions
+{
+    public const string SectionName = "Database";
+
+    public string ConnectionString { get; set; } = string.Empty;
+}
