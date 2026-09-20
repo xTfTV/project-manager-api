3998f65901859219bd78f6221c7c6ece48d15d86
xTfTV <mscott2003@icloud.com>
2026-09-20 17:11:43 -0400

    ft ✨ - Adjusting the endpoint to stop looking for userid and to use that of the bearer token of the user that is signed in

--- ProjectManager.Api/Controllers/ProjectController.cs
+++ ProjectManager.Api/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using ProjectManager.Api.Services;
 
@@ -17,8 +18,18 @@ public class ProjectsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll([FromQuery] int userId)
+    public async Task<IActionResult> GetAll()
     {
+        var userIdValue = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (!int.TryParse(userIdValue, out var userId))
+        {
+            return Unauthorized(new
+            {
+                message = "User Id claim is missing or invalid"
+            });
+        }
+
         var projects = await _projectService.GetAllProjectsAsync(userId);
 
         return Ok(projects);
