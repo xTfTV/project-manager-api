9897551c2802bd2ed8986995c46f09e9cd938b1f
xTfTV <mscott2003@icloud.com>
2026-09-20 20:15:29 -0400

    ft ✨ - Connecting these Update/Create operations to the API

--- ProjectManager.Api/Repositories/IProjectRepository.cs
+++ ProjectManager.Api/Repositories/IProjectRepository.cs
@@ -1,8 +1,13 @@
 using ProjectManager.Api.Models;
+using ProjectManager.Api.DTOs;
 
 namespace ProjectManager.Api.Repositories;
 
 public interface IProjectRepository
 {
     Task<IEnumerable<Project>> GetAllAsync(int createdByUserId);
+
+    Task<int> CreateAsync(CreateProjectRequest request, int createdByUserId);
+
+    Task<bool> UpdateAsync(int projectId, CreateProjectRequest request, int createdByUserId);
 }
